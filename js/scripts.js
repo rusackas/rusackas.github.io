f03f1a49ac489a996d36f05139e85b31c4ae8fb1
Evan Rusackas <evan@definitionstudio.com>
2014-10-15 21:09:17 -0700

    first bold swaths at mobile styles

--- js/scripts.js
+++ js/scripts.js
@@ -42,9 +42,7 @@ $(function () {
 
     if (
         rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) 
     ) return 'onscreen';
     else if(
       rect.top >= (window.innerHeight || document.documentElement.clientHeight) || rect.bottom <= 0
